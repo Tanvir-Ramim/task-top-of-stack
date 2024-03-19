f2a140a4239deb1ebd7959b23b86930739250c80
Tanvir Ramim <ramimhossan14@gmail.com>
2024-03-19 15:08:18 +0600

    all set up done

---
+++ src/MainPage/MainPage.jsx
@@ -0,0 +1,11 @@
+
+
+const MainPage = () => {
+    return (
+        <div className="text-5xl text-yellow-500">
+            <button className="btn btn-info">Info</button>
+        </div>
+    );
+};
+
+export default MainPage;
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import MainPage from './MainPage/MainPage.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MainPage></MainPage>
+  </React.StrictMode>,
+)
