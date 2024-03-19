08effe3dd9194ad8b75dea00e6ebd91fbb4cc2bc
Tanvir Ramim <ramimhossan14@gmail.com>
2024-03-19 19:55:50 +0600

    fotter done

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,50 @@
+
+
+const Footer = () => {
+    return (
+        <div className=" bg-blue-700 mt-16">
+        <div className="max-w-7xl m-auto">
+        <footer className="footer p-10 bg-blue-700  text-neutral-content">
+  <nav>
+    <h6 className="footer-title">Services</h6> 
+    <a className="link link-hover">Branding</a>
+    <a className="link link-hover">Design</a>
+    <a className="link link-hover">Marketing</a>
+    <a className="link link-hover">Advertisement</a>
+  </nav> 
+  <nav>
+    <h6 className="footer-title">Company</h6> 
+    <a className="link link-hover">About us</a>
+    <a className="link link-hover">Contact</a>
+    <a className="link link-hover">Jobs</a>
+    <a className="link link-hover">Press kit</a>
+  </nav> 
+  
+  <nav>
+    <h6 className="footer-title">Legal</h6> 
+    <a className="link link-hover">Terms of use</a>
+    <a className="link link-hover">Privacy policy</a>
+    <a className="link link-hover">Cookie policy</a>
+  </nav>
+  <nav>
+    <h6 className="footer-title">Legal</h6> 
+    <a className="link link-hover">Terms of use</a>
+    <a className="link link-hover">Privacy policy</a>
+    <a className="link link-hover">Cookie policy</a>
+  </nav>
+
+</footer>
+         <div>
+         <footer className="footer  footer-center p-4 bg-blue-700 text-white">
+  <aside>
+    <p>Copyright © 2024 - All right reserved by ACME Industries Ltd</p>
+  </aside>
+</footer>
+         </div>
+        </div>
+        
+        </div>
+    );
+};
+
+export default Footer;
--- src/MainPage/MainPage.jsx
+++ src/MainPage/MainPage.jsx
@@ -1,6 +1,7 @@
 import Banner from "../Components/Banner/Banner";
 import DealSection from "../Components/DealSection";
 import Featured from "../Components/Featured";
+import Footer from "../Components/Footer";
 import Navbar from "../Components/Navbar";
 import Newslatter from "../Components/Newslatter";
 import Testimonials from "../Components/Testimonials/Testimonials";
@@ -16,6 +17,7 @@ const MainPage = () => {
          <h1 className="max-w-7xl m-auto"><h1 className='text-3xl font-bold lg:mb-12 md:mb-10 mb-5 mt-8  md:mt-12 lg:mt-16 p-2 ml-2'>Testimonials</h1></h1>
          <Testimonials></Testimonials>
          <Newslatter></Newslatter>
+         <Footer></Footer>
         </div>
     );
 };
